04788237c4ae11ba36e2e8eb65de78b88e7d4212
Sean Olson <34286508+oldmanoldson@users.noreply.github.com>
2019-10-26 19:20:43 -0400

    added minutes to the time and rounded to nearest minute

--- evmaxx/evmaxx.r
+++ evmaxx/evmaxx.r
@@ -13,9 +13,9 @@ pal <- colorFactor(c("navy", "red"), domain = c("Public", "Private"))
 map <- leaflet() %>%
     addTiles() %>%
     setView(-93.65, 42.0285, zoom = 4) %>%
-    addCircleMarkers(data = df2, ~Longitude, ~Latitude, popup = ~paste("<b>",LocationTitle,"</b><br>","<b>Connection Type:</b> ",ConnectionType,"<br>","<b>Number of Points:</b> ",NumberOfPoints,"<br>","<b>Status:</b> ",StatusType, "<br><b>Reserve: </b>Enroute (",runif(1,min=5,max=60),")","<br> 10:00AM, 11:00AM, 12:00PM, 1:00PM, 2:00PM, 3:00PM, 4:00PM, 5:00PM"), clusterOptions = markerClusterOptions(),
+    addCircleMarkers(data = df2, ~Longitude, ~Latitude, popup = ~paste("<b>",LocationTitle,"</b><br>","<b>Connection Type:</b> ",ConnectionType,"<br>","<b>Number of Points:</b> ",NumberOfPoints,"<br>","<b>Status:</b> ",StatusType, "<br><b>Reserve: </b>Enroute (",round(runif(1,min=5,max=60),0),"min)","<br> 10:00AM, 11:00AM, 12:00PM, 1:00PM, 2:00PM, 3:00PM, 4:00PM, 5:00PM"), clusterOptions = markerClusterOptions(),
         color = "navy", group = "Public", fillOpacity = 0.5, stroke = FALSE) %>%
-    addCircleMarkers(data = df3, ~Longitude, ~Latitude, popup = ~paste("<b>",LocationTitle,"</b><br>","<b>Connection Type:</b> ",ConnectionType,"<br>","<b>Number of Points:</b> ",NumberOfPoints,"<br>","<b>Status:</b> ",StatusType, "<br><b>Reserve: </b>Enroute (",runif(1,min=5,max=60),")","<br> 10:00AM, 11:00AM, 12:00PM, 1:00PM, 2:00PM, 3:00PM, 4:00PM, 5:00PM"), clusterOptions = markerClusterOptions(),
+    addCircleMarkers(data = df3, ~Longitude, ~Latitude, popup = ~paste("<b>",LocationTitle,"</b><br>","<b>Connection Type:</b> ",ConnectionType,"<br>","<b>Number of Points:</b> ",NumberOfPoints,"<br>","<b>Status:</b> ",StatusType, "<br><b>Reserve: </b>Enroute (",round(runif(1,min=5,max=60),0),")","<br> 10:00AM, 11:00AM, 12:00PM, 1:00PM, 2:00PM, 3:00PM, 4:00PM, 5:00PM"), clusterOptions = markerClusterOptions(),
         color = "red", group = "Private", fillOpacity = 0.5, stroke = FALSE) %>%
     addMeasure(position = "bottomleft", primaryLengthUnit = "meters", primaryAreaUnit = "sqmeters", activeColor = "black", completedColor = "purple") %>%
     addScaleBar() %>%
